3726a333c431c372749cc704bc9cc5c14519a193
Abdullah_assoli <abdullahassoli@gmail.com>
2022-07-03 08:44:41 +0200

    change

--- src/components/Meals/MealsSummary.js
+++ src/components/Meals/MealsSummary.js
@@ -3,10 +3,10 @@ import classes from "./MealsSummary.module.css";
 const MealsSummary = () => {
   return (
     <section className={classes.summary}>
-      <h2>Amazing Background, Delivered To You</h2>
+      <h2>Amazing Background, To Your Food</h2>
       <p>
-        Choose your favorite Background from our Anime selection
-        and enjoy with amazing room.
+        Choose your favorite Background Meal from our Anime selection
+        and enjoy with amazing Food.
       </p>
       <p>
         All our Backgrounds are designed with high-quality, just-in-time and
